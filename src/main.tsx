9a8d537a5efbb22a4529cb05320979adb8450a2e
Nicole <nicoleho1314@gmail.com>
2025-05-07 23:23:15 +0100

    fix: fixing deploy issue

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-demo">
       <App />
     </BrowserRouter>
   </StrictMode>,
